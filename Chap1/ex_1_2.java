83ba99758fd38c6074f5ec6e93092a379d625f04
Muhammad Nurul Momen <39500000+Momen48@users.noreply.github.com>
2025-07-25 02:00:15 +0600

    Create ex_1_2.java

---
+++ Chap1/ex_1_2.java
@@ -0,0 +1,15 @@
+public class ex_1_2 {
+    public static void main(String[] args) {
+        byte maxByte = Byte.MAX_VALUE;
+        byte minByte = Byte.MIN_VALUE;
+        short maxValue = Short.MAX_VALUE;
+        short minValue = Short.MIN_VALUE;
+
+        System.out.println("Range of byte value is "+maxByte+" and "+minByte);
+        System.out.println("Range of short value is "+maxValue+" and "+minValue);
+
+        int maxInt = Integer.MAX_VALUE;
+        int minInt = Integer.MIN_VALUE;
+        System.out.println("Range of int value is "+maxInt+" and "+minInt);
+    }
+}
