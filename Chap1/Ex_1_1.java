39154d08066a18108fa3b1a21241bcb1200a35ce
Muhammad Nurul Momen <39500000+Momen48@users.noreply.github.com>
2025-07-25 01:59:58 +0600

    Create Ex_1_1.java

---
+++ Chap1/Ex_1_1.java
@@ -0,0 +1,9 @@
+public class Ex_1_1 {
+    public static void main(String[] args) {
+        int var1, var2;
+        var1 = 10;
+        var2 = 15;
+        int var3 = var1 + var2;
+        System.out.println("The addition of "+var1 +" and "+ var2 +" is = "+var3);
+    }
+}
